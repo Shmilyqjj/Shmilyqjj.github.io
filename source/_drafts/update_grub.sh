16f307ac4e2f646d5c686debba08ca56787b01e0
Shmilyqjj <710552907@qq.com>
2020-09-15 19:46:23 +0800

    manjaro更新系统后，需要替换grub内核参数  自动替换脚本

--- source/_drafts/update_grub.sh
+++ source/_drafts/update_grub.sh
@@ -9,15 +9,13 @@ fi
 
 GRUB_CFG="/boot/grub/grub.cfg"
 GRUB_CFG_BACKUP="$GRUB_CFG"_bak
-KERNEL_CONF='acpi_osi=! acpi_osi="Windows 2009"'
-BOOT_WAIT_TIMEOUT=3
 
 echo "Backup path: $GRUB_CFG_BACKUP"
 cp $GRUB_CFG $GRUB_CFG_BACKUP
 
-sed -i 's/quiet/quiet $KERNEL_CONF/g' $GRUB_CFG
+sed -i 's/quiet/quiet acpi_osi=! acpi_osi="Windows 2009"/g' $GRUB_CFG
 
-sed -i 's/timeout=10/timeout=$BOOT_WAIT_TIMEOUT/g' $GRUB_CFG
+sed -i 's/timeout=10/timeout=3/g' $GRUB_CFG
 
 echo "Result:"
 cat $GRUB_CFG | grep "quiet"
