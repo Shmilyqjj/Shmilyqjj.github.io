4c64a50fa38cbe9fa2181146591a2f61a411b0e4
Shmilyqjj <710552907@qq.com>
2020-12-08 00:48:25 +0800

    Intel+Nvidia双显卡 系统升级更新GRUB脚本

--- source/_drafts/update_grub.sh
+++ source/_drafts/update_grub.sh
@@ -13,11 +13,15 @@ GRUB_CFG_BACKUP="$GRUB_CFG"_bak
 echo "Backup path: $GRUB_CFG_BACKUP"
 cp $GRUB_CFG $GRUB_CFG_BACKUP
 
-# 防止不能加载显卡不能进桌面
-sed -i 's/quiet/quiet acpi_osi=! acpi_osi="Windows 2009"/g' $GRUB_CFG
-# 开机等待界面超时时间设为3s
-sed -i 's/timeout=10/timeout=3/g' $GRUB_CFG
-
+if [ -n "$(grep "Windows 2009" $GRUB_CFG)" ]; then
+  echo "Grub config is ok,no update."
+else
+  echo "Grub config need to be updated.Go to update it."
+  # 防止不能加载显卡不能进桌面
+  sed -i 's/quiet/quiet acpi_osi=! acpi_osi="Windows 2009"/g' $GRUB_CFG
+  # 开机等待界面超时时间设为3s
+  sed -i 's/timeout=10/timeout=3/g' $GRUB_CFG
+fi
 echo "Result:"
 cat $GRUB_CFG | grep "quiet"
 cat $GRUB_CFG | grep "timeout="
