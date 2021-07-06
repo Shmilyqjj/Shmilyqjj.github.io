23a74d4ba2442289d75f99b96ffb36b0d1445e40
shmily <710552907@qq.com>
2021-07-07 01:24:33 +0800

    Manjaro Deploy

--- source/_drafts/update_grub.sh
+++ source/_drafts/update_grub.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
-
 echo "双显卡笔记本更新Manjaro系统后需要添加grub参数避免无法开机"
-
 if [[ $(whoami) != root ]]; then
   echo -e "\033[41;37m[ERROR] Need sudo or root privilege.\033[0m"
   exit 1
 fi
-
 GRUB_CFG="/boot/grub/grub.cfg"
 GRUB_CFG_BACKUP="$GRUB_CFG"_bak
-
 echo "Backup path: $GRUB_CFG_BACKUP"
 cp $GRUB_CFG $GRUB_CFG_BACKUP
-
 if [ -n "$(grep "Windows 2009" $GRUB_CFG)" ]; then
   echo "Grub config is ok,no update."
 else
@@ -22,8 +17,6 @@ else
   # 开机等待界面超时时间设为3s
   sed -i 's/timeout=10/timeout=3/g' $GRUB_CFG
 fi
-echo "Result:"
+echo -e "\033[42;3mAll Done. Result:\033[0m"
 cat $GRUB_CFG | grep "quiet"
 cat $GRUB_CFG | grep "timeout="
-
-echo -e "\033[42;3mAll Done.\033[0m"
