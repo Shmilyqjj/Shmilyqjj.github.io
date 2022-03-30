2c57b8beddc1c8053089efbe13c37a125b170d94
shmily <710552907@qq.com>
2022-03-30 18:30:15 +0800

    add cdn refresh tool

---
+++ Tools/JsdelivrCacheRefresh.py
@@ -0,0 +1,10 @@
+import requests
+
+photo_jsdelivr_url = "https://cdn.jsdelivr.net/gh/Shmilyqjj/BlogImages-0@master/cdn_sources/Blog_Images/Bigdata-Problems/HBase/HBase-Problems-31.png"
+
+
+res = requests.get(photo_jsdelivr_url.replace('cdn', 'purge'))
+if res.status_code == 200:
+    print("Jsdelivr图片缓存刷新完成")
+    print(res.text)
+
